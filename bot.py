6ffc7d2d87c6a4329d84636a2fef455ad61d7c4d
DanyNHJD <dany8079@gmail.com>
2020-12-22 16:00:30 -0600

    Create local repository

---
+++ bot.py
@@ -0,0 +1,70 @@
+import discord
+import random
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+#Load token from .env file and set as variable
+load_dotenv()
+TOKEN = os.getenv('DANYBOT_TOKEN')
+
+#Sets prefix for bot
+danybot = commands.Bot(command_prefix = 'd!')
+
+#Loads up cogs (allow putting commands on other files)
+@danybot.command()
+async def load(ctx, extension):
+    danybot.load_extension(f'cogs.{extension}')
+    await ctx.send(f'Loaded {extension}.')
+
+@danybot.command()
+async def unload(ctx, extension):
+    danybot.unload_extension(f'cogs.{extension}')
+    await ctx.send(f'Unloaded {extension}.')
+
+@danybot.command()
+async def reload(ctx, extension):
+    danybot.unload_extension(f'cogs.{extension}')
+    danybot.load_extension(f'cogs.{extension}.')
+    await ctx.send(f'Reloaded {extension}.')
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        danybot.load_extension(f'cogs.{filename[:-3]}')
+
+@danybot.event
+async def on_ready():
+    print("DanyBot is now ready.")
+
+#Ping test command
+@danybot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(danybot.latency * 1000)}ms')
+
+@danybot.command(aliases=['8ball'])
+async def eightball(ctx, *, question):
+    responses = [
+    "It is certain",
+    "Without a doubt",
+    "You may rely on it",
+    "Yes definitely",
+    "It is decidedly so",
+    "As I see it, yes",
+    "Most likely",
+    "Yes",
+    "Outlook good",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Better not tell you now",
+    "Ask again later",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "Outlook not so good",
+    "My sources say no",
+    "Very doubtful",
+    "My reply is no"]
+    await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
+
+danybot.run(TOKEN)
---
+++ cogs/Admin.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+
+class Admin(commands.Cog):
+
+    def __init__(self, danybot):
+        self.danybot = danybot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Admin.py is loaded.')
+
+    @commands.command()
+    async def clear(self, ctx, amount=5):
+        clear_messages = int(amount)
+        await ctx.channel.purge(limit=clear_messages+1)
+
+    @commands.command()
+    async def kick(self, ctx, member : discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+        await ctx.send(f'Kicked {member.mention}')
+
+    @commands.command()
+    async def ban(self, ctx, member : discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+        await ctx.send(f'Banned {member.mention}')
+
+    @commands.command()
+    async def unban(self, ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'Unbanned {user.name}#{user.discriminator}')
+                return
+
+def setup(danybot):
+    danybot.add_cog(Admin(danybot))
