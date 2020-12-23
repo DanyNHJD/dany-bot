f7038f4f06172018abee4b99a4c13fea18625222
DanyNHJD <dany8079@gmail.com>
2020-12-23 12:47:32 -0600

    beta music player, has lots of bugs.

--- cogs/Audio.py
+++ cogs/Audio.py
@@ -1,7 +1,28 @@
 import discord
+import youtube_dl
+import os
+import asyncio
 from discord.ext import commands
 from discord.utils import get
+from discord.voice_client import VoiceClient
+from discord.ext.commands import Bot
 import urllib.parse, urllib.request, re
+from discord import FFmpegPCMAudio
+from os import system
+
+# Configure settings for youtube-dl
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+def endSong(guild, path):
+    os.remove(path)
+
 
 class Audio(commands.Cog):
 
@@ -12,16 +33,40 @@ class Audio(commands.Cog):
     async def on_ready(self):
         print('Audio.py is loaded.')
 
-    @commands.command(aliases=['play'])
-    async def yt(self, ctx, *, search):
+    @commands.command(aliases=['yt'], brief="Play music from youtube in a voice channel.")
+    async def play(self, ctx, *, search=None):
+        if not ctx.message.author.voice:
+            return await ctx.send(f'You need to join a voice chat {ctx.author.mention}!')
+        if search == None:
+            return await ctx.send(f'{ctx.author.mention}, Please type in a video name / URL.')
 
+        channel = ctx.message.author.voice.channel
+        voice_client = await channel.connect()
+        guild = ctx.message.guild
+        # Look for song then save it to the url variable
         query_string = urllib.parse.urlencode({'search_query': search})
         htm_content = urllib.request.urlopen(
             'http://www.youtube.com/results?' + query_string)
         search_results = re.findall(r'/watch\?v=(.{11})',
                                     htm_content.read().decode())
-        await ctx.send('http://www.youtube.com/watch?v=' + search_results[0])
-        await ctx.send('Currently setting up the ability to play music in voice chat, hang tight!')
+        url = 'http://www.youtube.com/watch?v=' + search_results[0]
+
+        print(url)
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            file = ydl.extract_info(url, download=True)
+            path = str(file['title']) + "-" + str(file['id'] + ".mp3")
+
+        voice_client.play(discord.FFmpegPCMAudio(path), after=lambda x: endSong(guild, path))
+        voice_client.source = discord.PCMVolumeTransformer(voice_client.source, 1)
+
+        await ctx.send(f'**Music: **{url}')
+
+        while voice_client.is_playing():
+            await asyncio.sleep(1)
+        else:
+            await voice_client.disconnect()
+            print(f'Disconnected from {ctx.guild.name}')
+
 
 def setup(danybot):
     danybot.add_cog(Audio(danybot))
